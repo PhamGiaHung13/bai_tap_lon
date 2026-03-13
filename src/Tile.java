a51e2ed809e8404295bcd6d5fbdb6fcd6189da9c
Hungchimto <giahungpgh13@gmail.com>
2026-03-13 11:16:03 +0700

    add board and tile code

---
+++ src/Tile.java
@@ -0,0 +1,39 @@
+public class Tile {
+
+    int row;
+    int col;
+
+    boolean mine = false;
+    boolean revealed = false;
+
+    int minesAround = 0;
+
+    public Tile(int r, int c) {
+        this.row = r;
+        this.col = c;
+    }
+
+    public boolean isMine() {
+        return mine;
+    }
+
+    public void setMine(boolean mine) {
+        this.mine = mine;
+    }
+
+    public boolean isRevealed() {
+        return revealed;
+    }
+
+    public void setRevealed(boolean revealed) {
+        this.revealed = revealed;
+    }
+
+    public void setMinesAround(int count) {
+        this.minesAround = count;
+    }
+
+    public int getMinesAround() {
+        return minesAround;
+    }
+}
