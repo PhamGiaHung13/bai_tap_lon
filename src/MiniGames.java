84b65e88dfb15d0f40eae41638aca2675f85cab5
Hungchimto <giahungpgh13@gmail.com>
2026-03-18 14:43:02 +0700

    add SudokuGame and MiniGames

--- src/MiniGames.java
+++ src/MiniGames.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class MiniGames {
 
@@ -46,14 +47,51 @@ public class MiniGames {
         return true;
     }
 
-    // ======================
-    // Sudoku
-    // ======================
+    // ================== SUDOKU GAME ==================
     boolean sudokuGame(){
 
-        SudokuGame sudoku = new SudokuGame(difficulty);
+        SudokuGame game = new SudokuGame(difficulty);
 
-        // tạm thời auto thắng
-        return true;
+        int maxErrors;
+        if(difficulty == 1) maxErrors = 5;
+        else if(difficulty == 2) maxErrors = 3;
+        else maxErrors = 2;
+
+        while(true){
+
+            game.printPuzzle();
+
+            int[][] board = game.getPuzzle();
+            int size = game.getSize();
+
+            // tìm ô trống
+            int r = -1, c = -1;
+            for(int i=0;i<size;i++){
+                for(int j=0;j<size;j++){
+                    if(board[i][j] == 0){
+                        r = i;
+                        c = j;
+                        break;
+                    }
+                }
+                if(r != -1) break;
+            }
+
+            if(r == -1) return true; // WIN
+
+            Scanner sc = new Scanner(System.in);
+
+            System.out.println("Sudoku: ["+r+"]["+c+"] = ");
+            int guess = sc.nextInt();
+
+            if(!game.move(r,c,guess)){
+                maxErrors--;
+                System.out.println("Sai! Con " + maxErrors);
+
+                if(maxErrors == 0){
+                    return false; // LOSE
+                }
+            }
+        }
     }
 }
--- src/SudokuGame.java
+++ src/SudokuGame.java
@@ -1,68 +1,165 @@
+import java.util.Random;
+
 public class SudokuGame {
 
-    int[][] solution;
     int size;
+    int[][] solution;
+    int[][] puzzle;
+
+    int errors = 0;
+    Random rand = new Random();
 
     public SudokuGame(int difficulty){
 
-        if(difficulty == 1){
+        if(difficulty == 1) size = 4;
+        else if(difficulty == 2) size = 6;
+        else size = 9;
 
-            size = 4;
+        solution = new int[size][size];
+        puzzle = new int[size][size];
 
-            solution = new int[][]{
-                    {1,2,3,4},
-                    {3,4,1,2},
-                    {2,1,4,3},
-                    {4,3,2,1}
-            };
-        }
-
-        else if(difficulty == 2){
-
-            size = 6;
-
-            solution = new int[][]{
-                    {1,2,3,4,5,6},
-                    {4,5,6,1,2,3},
-                    {2,3,1,5,6,4},
-                    {5,6,4,2,3,1},
-                    {3,1,2,6,4,5},
-                    {6,4,5,3,1,2}
-            };
-        }
-
-        else{
-
-            size = 9;
-
-            solution = new int[][]{
-                    {5,3,4,6,7,8,9,1,2},
-                    {6,7,2,1,9,5,3,4,8},
-                    {1,9,8,3,4,2,5,6,7},
-                    {8,5,9,7,6,1,4,2,3},
-                    {4,2,6,8,5,3,7,9,1},
-                    {7,1,3,9,2,4,8,5,6},
-                    {9,6,1,5,3,7,2,8,4},
-                    {2,8,7,4,1,9,6,3,5},
-                    {3,4,5,2,8,6,1,7,9}
-            };
-        }
+        generate(0, 0);          // sinh solution
+        copy(solution, puzzle);
+        removeCells(difficulty); // tạo puzzle theo độ khó
     }
 
-    public boolean move(int r,int c,int value){
+    // ================== SINH SOLUTION ==================
+    boolean generate(int r, int c){
 
-        if(solution[r][c] == value){
-            return true;
+        if(r == size) return true;
+
+        int nextR = (c == size - 1) ? r + 1 : r;
+        int nextC = (c + 1) % size;
+
+        int[] nums = shuffle();
+
+        for(int num : nums){
+            if(isSafe(r, c, num)){
+                solution[r][c] = num;
+
+                if(generate(nextR, nextC)) return true;
+            }
         }
 
+        solution[r][c] = 0;
         return false;
     }
 
-    public int getSize(){
-        return size;
+    int[] shuffle(){
+        int[] arr = new int[size];
+        for(int i=0;i<size;i++) arr[i] = i+1;
+
+        for(int i=0;i<size;i++){
+            int j = rand.nextInt(size);
+            int tmp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = tmp;
+        }
+        return arr;
     }
 
-    public int[][] getSolution(){
-        return solution;
+    boolean isSafe(int r, int c, int val){
+
+        for(int i=0;i<size;i++){
+            if(solution[r][i] == val || solution[i][c] == val){
+                return false;
+            }
+        }
+
+        if(size == 6){
+            int boxR = (r/2)*2;
+            int boxC = (c/3)*3;
+
+            for(int i=0;i<2;i++){
+                for(int j=0;j<3;j++){
+                    if(solution[boxR+i][boxC+j] == val){
+                        return false;
+                    }
+                }
+            }
+        } else {
+            int box = (int)Math.sqrt(size);
+            int boxR = (r/box)*box;
+            int boxC = (c/box)*box;
+
+            for(int i=0;i<box;i++){
+                for(int j=0;j<box;j++){
+                    if(solution[boxR+i][boxC+j] == val){
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
     }
+
+    void copy(int[][] a, int[][] b){
+        for(int i=0;i<size;i++){
+            for(int j=0;j<size;j++){
+                b[i][j] = a[i][j];
+            }
+        }
+    }
+
+    // ================== TẠO PUZZLE ==================
+    void removeCells(int difficulty){
+
+        int remove;
+
+        if(difficulty == 1) remove = size * size / 4;
+        else if(difficulty == 2) remove = size * size / 2;
+        else remove = size * size * 3 / 4;
+
+        while(remove > 0){
+            int r = rand.nextInt(size);
+            int c = rand.nextInt(size);
+
+            if(puzzle[r][c] != 0){
+                puzzle[r][c] = 0;
+                remove--;
+            }
+        }
+    }
+
+    // ================== MOVE ==================
+    public boolean move(int r, int c, int val){
+
+        if(puzzle[r][c] != 0) return false;
+
+        if(solution[r][c] == val){
+            puzzle[r][c] = val;
+            return true;
+        } else {
+            errors++;
+            return false;
+        }
+    }
+
+    // ================== CHECK WIN ==================
+    public boolean isWin(){
+        for(int i=0;i<size;i++){
+            for(int j=0;j<size;j++){
+                if(puzzle[i][j] == 0) return false;
+            }
+        }
+        return true;
+    }
+
+    public void printPuzzle(){
+        for(int i=0;i<size;i++){
+            for(int j=0;j<size;j++){
+                if(puzzle[i][j] == 0){
+                    System.out.print(". ");
+                } else {
+                    System.out.print(puzzle[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public int[][] getPuzzle(){ return puzzle; }
+    public int[][] getSolution(){ return solution; }
+    public int getSize(){ return size; }
 }
