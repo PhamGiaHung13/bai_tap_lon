19a6cb272f5d3178be66c3e1092077f8a9d11f73
KhaiKo <trietay2020@gmail.com>
2026-04-03 10:19:36 +0700

    not finished update

--- src/core/UI/GameFrame.java
+++ src/core/UI/GameFrame.java
@@ -2,27 +2,50 @@
     import javax.swing.*;
     
     import core.Logic.Board;
+
+    import java.awt.*;
+
     //cua so game
     public class GameFrame extends JFrame {
+
+        private CardLayout cardLayout;
+        private JPanel mainPanel;
+
+
+
         public GameFrame() {
-            setTitle("Minesweeper");//dat tieu de
-            setSize(600, 600);//kich thuoc cua so
+            setTitle("HKL Minesweeper");//dat tieu de
+            setSize(1200, 700);//kich thuoc cua so
             setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//an nut X thoat game(khong cho game chay ngam)
+
+            cardLayout = new CardLayout();
+            mainPanel = new JPanel(cardLayout);
+
+
+            mainPanel.add(new MenuPanel(this), "MENU");
+
+
             setLocationRelativeTo(null);//can cua so ra giua man hinh
-            showMenu();//hien menu panel
-    
+            setContentPane(mainPanel);
             setVisible(true);//hien thi cua so khong cho no tang hinh
         }
+
     
-        public void showMenu() {
-            setContentPane(new MenuPanel(this));//xuat menu panel
-            revalidate();//refresh UI
-        }
-    
-        public void startGame() {
-            Board newBoard = new Board(1);
-            setContentPane(new GamePanel(newBoard));//thay menu bang game panel
-            revalidate();
+        public void startGame(int mode) {
+            Board newBoard = new Board(mode);
+
+            GamePanel gamePanel = new GamePanel(newBoard);
+
+            int tileSize = 40;
+
+            int width = newBoard.columns * tileSize + 100;
+            int height = newBoard.rows * tileSize + 150;
+
+            setSize(width, height);
+
+            mainPanel.add(gamePanel, "GAME");
+            cardLayout.show(mainPanel, "GAME");
+
         }
     
     }
--- src/core/UI/GamePanel.java
+++ src/core/UI/GamePanel.java
@@ -6,6 +6,7 @@ import core.Logic.*;
 import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
+import java.util.ArrayList;
 
 
 ///   ------- LAYOUT PLAY GAME
@@ -19,14 +20,17 @@ public class GamePanel extends JPanel {
     private int tileSize;
     JLabel minesLabel;
     JButton faceBtn;
-    ImageIcon smileIcon, deadIcon, winIcon;
+    ImageIcon smileIcon, deadIcon, winIcon, surpriseIcon = getScaledIcon("src/Image/surprise.png", 40);
     JLabel timerLabel;
     int time = 0;
     Timer gameTimer;
     boolean started = false;
+    boolean isHolding = false;
+
+
 
 /// ---------- CONSTRUCTOR
-    public GamePanel(Board board){
+    public GamePanel(Board board) {
 
 
         ///--------------- LAYOUT GAMEPANEL
@@ -40,55 +44,55 @@ public class GamePanel extends JPanel {
         ));
 
 
-
-
         // ---------- 1.LABEL SO MIN CON LAI
         minesLabel = new JLabel("000");
-        minesLabel.setFont(new Font("Consolas", Font.BOLD, 40));
+        minesLabel.setFont(new Font("Consolas", Font.BOLD, 32));
         minesLabel.setForeground(Color.RED);
         minesLabel.setBackground(Color.black);
         minesLabel.setOpaque(true);
         minesLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        minesLabel.setPreferredSize(new Dimension(50, 50));
+        minesLabel.setPreferredSize(new Dimension(100, 50));
         minesLabel.setBorder(BorderFactory.createCompoundBorder(createThickFrame(false, 3)
-        ,BorderFactory.createEmptyBorder(20, 10, 5, 10))
+                , BorderFactory.createEmptyBorder(5, 0, 0, 0))
         );
-
-
-
 
 
         //------- 2.BUTTON FACE ICON
         faceBtn = new JButton();
         faceBtn.setFocusPainted(false);
-        faceBtn.setBackground(new Color(192,192,192));
+        faceBtn.setBackground(new Color(192, 192, 192));
         faceBtn.setBorder(createThickFrame(true, 4));
-        faceBtn.setPreferredSize(new Dimension(60,60));
+        faceBtn.setPreferredSize(new Dimension(200, 60));
 
         smileIcon = getScaledIcon("src/Image/smile.png", 40);
         deadIcon = getScaledIcon("src/Image/die.png", 40);
         winIcon = getScaledIcon("src/Image/cool.png", 40);
 
         faceBtn.setIcon(smileIcon);
+        faceBtn.addActionListener(e -> {
+            faceBtn.setBorder(createThickFrame(false, 4));
+            new Timer(100, ev ->{
+                restartGame();
+                faceBtn.setBorder(createThickFrame(true, 4));
+                ((Timer) ev.getSource()).stop();
+            }).start();
+        });
 
         JPanel centerPanel = new JPanel(new GridBagLayout());
         centerPanel.setOpaque(false);
         centerPanel.add(faceBtn);
 
 
-
-
-
         //------ 3.TIMER
         timerLabel = new JLabel("000");
-        timerLabel.setFont(new Font("Consolas", Font.BOLD, 40));
+        timerLabel.setFont(new Font("Consolas", Font.BOLD, 32));
         timerLabel.setForeground(Color.RED);
         timerLabel.setBackground(Color.BLACK);
         timerLabel.setOpaque(true);
         timerLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        timerLabel.setPreferredSize(new Dimension(50, 50));
+        timerLabel.setPreferredSize(new Dimension(100, 50));
         timerLabel.setBorder(BorderFactory.createCompoundBorder(createThickFrame(false, 3)
-                ,BorderFactory.createEmptyBorder(20, 10, 5, 10))
+                , BorderFactory.createEmptyBorder(5, 0, 0, 0))
         );
 
         gameTimer = new Timer(1000, e -> {
@@ -97,10 +101,8 @@ public class GamePanel extends JPanel {
         });
 
 
-
-
         /// ---------- TOOLBAR PANEL
-        JPanel toolBar = new JPanel(new GridLayout(1, 3, 20, 0));
+        JPanel toolBar = new JPanel(new BorderLayout());
         toolBar.setBackground(new Color(192, 192, 192));
         toolBar.setPreferredSize(new Dimension(0, 90));
         toolBar.setBorder(BorderFactory.createCompoundBorder(
@@ -110,10 +112,11 @@ public class GamePanel extends JPanel {
                 ),
                 BorderFactory.createEmptyBorder(0, 0, 0, 0) // Margin DUOI
         ));
-        toolBar.add(minesLabel);
-        toolBar.add(centerPanel);
-        toolBar.add(timerLabel);
 
+
+        toolBar.add(minesLabel, BorderLayout.WEST);
+        toolBar.add(centerPanel, BorderLayout.CENTER);
+        toolBar.add(timerLabel, BorderLayout.EAST);
 
 
         ///---------- layout BOARD(gridlayout la layout theo ma tran)
@@ -126,15 +129,14 @@ public class GamePanel extends JPanel {
         ));
 
 
-
         //-------- SAP XEP VI TRI BOARD PANEL VA TOOLBAR PANEL
         add(toolBar, BorderLayout.NORTH);
         add(boardPanel, BorderLayout.CENTER);
 
 
-        ///-------- LISTENER RESIZE
+        ///-------- LISTENER RESIZE TILE THEO BOARD
         // COMPONENT
-        boardPanel.addComponentListener(new java.awt.event.ComponentAdapter(){
+        boardPanel.addComponentListener(new java.awt.event.ComponentAdapter() {
             public void componentResized(java.awt.event.ComponentEvent e) {
                 updateTileSize();
             }
@@ -142,90 +144,138 @@ public class GamePanel extends JPanel {
 
 
         ///------- FUNCTION PLAY GAME
-        for(int r = 0; r < board.rows; r++){
-            for(int c = 0; c < board.columns; c++){
+        for (int r = 0; r < board.rows; r++) {
+            for (int c = 0; c < board.columns; c++) {
 
-                Tile tile = board.getTile(r,c);
+                Tile tile = board.getTile(r, c);
 
                 tile.setFocusPainted(false);
                 tile.setOpaque(true);
 
-
-                /// ------ MOUSE LISTENER
-                tile.addMouseListener(new java.awt.event.MouseAdapter(){
-
-
-                    //--- PUT FLAG (RIGHT CLICK)
-                    @Override
-                    public void mousePressed(java.awt.event.MouseEvent e){
-                        if(board.gameOver) return;
-                        if(SwingUtilities.isRightMouseButton(e)) {
-                            if(tile.isRevealed()) return;
-                            tile.setFlagged(!tile.isFlagged());
-                            updateUIBoard();
-                        }
-                    }
-                    public void mouseReleased(){
-
-                    }
-
-                    //--------- HOVER (in and out)
-                    public void mouseEntered(java.awt.event.MouseEvent evt){
-                        if(board.gameOver) return;
-                        if(!tile.isRevealed()){
-                            tile.setBackground(new Color(210, 210, 210));
-                        }
-                    }
-                    public void mouseExited(java.awt.event.MouseEvent evt){
-                        if(board.gameOver) return;
-                        if(!tile.isRevealed()){
-                            tile.setBackground(new Color(192,192,192));
-                        }
-                    }
-                });
-
-                //------- ACTION LISTENER (click)
-                tile.addActionListener(e -> {
-
-                    if(!started){
-                        gameTimer.start();
-                        started = true;
-                    }
-
-                    if(board.gameOver || tile.isFlagged()){
-                        return;
-                    }
-
-                    //----- LEFT CLICK
-                    Events.ClickResult result = events.clickTile(tile, () -> {
-                        updateUIBoard();
-
-                        //----- WIN
-                        if(board.isWin()){
-                            gameTimer.stop();
-                            JOptionPane.showMessageDialog(this, "YOU WIN!");
-                        }
-                    });
-
-                    //---------- HIEU UNG BOM NHAP NHAY
-                    int[] count = {0};  //  -----  mang 1 phan tu de dem so lan nhap nhay bom
-                    if(result == Events.ClickResult.MINE){
-                        gameTimer.stop();
-
-                        new javax.swing.Timer(100,i ->{
-                            tile.setBackground(tile.getBackground() == Color.RED ? Color.WHITE : Color.RED);
-                            count[0]++;
-                            if(count[0] > 10) ((Timer)i.getSource()).stop();
-                        }).start();
-
-                        revealAllMines();// ---- hien tat ca bom khi thua
-                    }
-                    updateUIBoard();// --- cap nhat board
-                });
-
+                /// ----------------- GAMEPLAY
+                addTileListener(tile);
                 boardPanel.add(tile);
             }
         }
+        addComponentListener(new java.awt.event.ComponentAdapter() {
+            @Override
+            public void componentResized(java.awt.event.ComponentEvent e) {
+                repaint();
+            }
+        });
+
+        /// ---------
+    }
+
+
+
+
+    ///  ----------------- TILE LISTENER
+    private void addTileListener(Tile tile){
+
+        /// ------ MOUSE LISTENER
+        tile.addMouseListener(new java.awt.event.MouseAdapter(){
+
+
+            //-------- HOLD MOUSE
+            @Override
+            public void mousePressed(java.awt.event.MouseEvent e){
+                if(board.gameOver) return;
+
+                // --------- FACE EMOTION (LEFT CLICK)
+                if(SwingUtilities.isLeftMouseButton(e)){
+                    isHolding = true;
+                    faceBtn.setIcon(surpriseIcon);
+                    handlePreview(tile);
+                }
+
+                //--- PUT FLAG (RIGHT CLICK)
+                if(SwingUtilities.isRightMouseButton(e)) {
+                    if(tile.isRevealed()) return;
+                    tile.setFlagged(!tile.isFlagged());
+                    updateUIBoard();
+                }
+            }
+
+            //----------- RELEASE MOUSE
+            @Override
+            public void mouseReleased(java.awt.event.MouseEvent e){
+                isHolding = false;
+                updateFace(); // ------- TRA VE TRANG THAI FACE
+                clearPreview();// ------- CLEAR PREVIEW
+            }
+
+            //--------- HOVER (in and out)
+            @Override
+            public void mouseEntered(java.awt.event.MouseEvent evt){
+                if(board.gameOver) return;
+
+                if(isHolding){
+                    handlePreview(tile);
+                }else {
+                    if (!tile.isRevealed()) {
+                        tile.setBackground(new Color(210, 210, 210));
+                    }
+                }
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt){
+                if(board.gameOver) return;
+                if(!tile.isRevealed()){
+                    tile.setBackground(new Color(192,192,192));
+                }
+            }
+        });
+
+        //------- ACTION LISTENER (click)
+        tile.addActionListener(e -> {
+
+            if(!started){
+                gameTimer.start();
+                started = true;
+            }
+
+            if(board.gameOver || tile.isFlagged()){
+                return;
+            }
+
+            //----- LEFT CLICK
+            Events.ClickResult result = events.clickTile(tile, () -> {
+                updateUIBoard();
+
+                //----- WIN
+                if(board.isWin()){
+                    gameTimer.stop();
+                    playWinAnimation();
+                    JOptionPane.showMessageDialog(this, "YOU WIN!");
+                }
+            });
+
+            //---------- HIEU UNG BOM NHAP NHAY
+            int[] count = {0};  //  -----  mang 1 phan tu de dem so lan nhap nhay bom
+            if(result == Events.ClickResult.MINE){
+                gameTimer.stop();
+
+                new javax.swing.Timer(100,i ->{
+                    tile.setBackground(tile.getBackground() == Color.RED ? Color.WHITE : Color.RED);
+                    count[0]++;
+                    if(count[0] > 10) ((Timer)i.getSource()).stop();
+                }).start();
+
+                revealAllMines();// ---- hien tat ca bom khi thua
+            }
+            updateUIBoard();// --- cap nhat board
+        });
+    }
+
+
+
+
+
+    ///  ---------- UPDATE FACE
+    private void updateFace(){
+        if(board.gameOver) faceBtn.setIcon(deadIcon);
+        else if(board.isWin()) faceBtn.setIcon(winIcon);
+        else faceBtn.setIcon(smileIcon);
     }
 
 
@@ -291,7 +341,8 @@ public class GamePanel extends JPanel {
         };
     }
 
-//---------- UPDATE MINELEFT ----
+
+///---------- UPDATE MINELEFT ----
     private void updateMinesCounter(){
         int flags = 0;
         for(int r = 0; r < board.rows; r++)
@@ -327,10 +378,62 @@ public class GamePanel extends JPanel {
 
                 tile.setPreferredSize(new Dimension(tileSize, tileSize));
 
-                tile.setFont(new Font("Segoe UI", Font.BOLD, tileSize / 3));
+                tile.setFont(new Font("Segoe UI", Font.BOLD, tileSize * 3/4));
             }
         updateUIBoard();
     }
+
+
+
+
+
+
+    /// ------- HANDLE PREVIEW TILE
+    private java.util.List<Tile> previewTiles = new ArrayList<>();
+    private void handlePreview(Tile tile){
+        if(board.gameOver) return;
+        clearPreview();
+
+        if(!tile.isRevealed()){
+            tile.setBorder(createThickFrame(false, 4));
+            tile.setBackground(new Color(220,220,220));
+            previewTiles.add(tile);
+            return;
+        }
+
+        if(tile.getMinesAround() > 0){
+            for(int i=-1;i<=1;i++)
+                for(int j=-1;j<=1;j++)
+                    if(i!=0 || j!=0){
+                        int nr = tile.row + i;
+                        int nc = tile.col + j;
+
+                        if(nr>=0 && nr< board.rows && nc>=0 && nc< board.columns)
+                            if(!board.getTile(nr, nc).isRevealed()){
+                                board.getTile(nr, nc).setBorder(createThickFrame(false, 4));
+                                board.getTile(nr, nc).setBackground(new Color(220,220,220));
+                                previewTiles.add(board.getTile(nr, nc));
+                            }
+                    }
+        }
+    }
+
+
+
+
+
+    ///  --------- CLEAR PREVIEW
+    private void clearPreview(){
+        for(Tile t : previewTiles){
+            if(!t.isRevealed()){
+                t.setBorder(createThickFrame(true, 5));
+                t.setBackground(new Color(192,192,192));
+            }
+        }
+        previewTiles.clear();
+    }
+
+
 
 
 
@@ -367,12 +470,76 @@ public class GamePanel extends JPanel {
 
 
 
+
+    /// --------- FACE BUTTON RESTART GAME
+    private void restartGame(){
+        this.board = new Board(board.rows, board.columns, board.minesCount);
+        this.events = new Events(board);
+
+        gameTimer.stop();
+        time = 0;
+        started = false;
+        timerLabel.setText("000");
+
+        faceBtn.setIcon(smileIcon);
+
+        boardPanel.removeAll();
+
+        for(int r = 0; r < board.rows; r++){
+            for(int c = 0; c < board.columns; c++){
+
+                Tile tile = board.getTile(r,c);
+
+                tile.setFocusPainted(false);
+                tile.setOpaque(true);
+
+                addTileListener(tile);
+                boardPanel.add(tile);
+            }
+        }
+        updateUIBoard();
+
+    }
+
+
+
+
+
+    /// ---------- WIN ANIMATION
+    private void playWinAnimation(){
+        Timer winTimer = new Timer(120, null);
+
+        final boolean[] toggle = {false};
+        final int[] count = {0};
+
+        winTimer.addActionListener(e -> {
+            toggle[0] = !toggle[0];
+
+            for(int r = 0; r < board.rows; r++){
+                for(int c = 0; c < board.columns; c++){
+                    Tile tile = board.getTile(r, c);
+
+                    if(toggle[0]) tile.setBackground(Color.CYAN);
+                    else tile.setBackground(Color.green);
+                }
+            }
+
+            count[0]++;
+            if(count[0] >= 10) {
+                winTimer.stop();
+                updateUIBoard();
+            }
+        });
+        winTimer.start();
+    }
+
+
+
+
+
     /// ---------- UPDATE BOARD
     public void updateUIBoard(){
-
-        if(board.gameOver) faceBtn.setIcon(deadIcon);
-        else if(board.isWin()) faceBtn.setIcon(winIcon);
-        else faceBtn.setIcon(smileIcon);
+        updateFace();
         updateMinesCounter();
 
         for(int r = 0; r < board.rows; r++)
@@ -416,9 +583,12 @@ public class GamePanel extends JPanel {
                 else{
                     tile.setBackground(new Color(192,192,192));
                     tile.setBorder(createThickFrame(true, 5));
-                    if(tile.isFlagged()){
+                    if(tile.isFlagged()) {
                         tile.setIcon(flagIcon);
-                        if(board.gameOver && !tile.isMine()) tile.setBackground(new Color(240, 70, 70));
+                        if (board.gameOver && !tile.isMine()) {
+                            tile.setBackground(new Color(240, 70, 70));
+//                            tile.setBorder(null);
+                        }
 
                     }
                 }
